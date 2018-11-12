d43c0c79240b5aa1d3afd61087e0188237d4214e
SangJun-HitHub <sjpark24@myseneca.ca>
2018-11-11 19:50:32 -0500

    commit

---
+++ _01MyFrame.java
@@ -0,0 +1,19 @@
+package practice;
+
+import javax.swing.*;
+
+/**
+ * Created by bobsang89@gmail.com on 2018-11-11
+ * Project: JAC444
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class _01MyFrame {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Test Frame");
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+
